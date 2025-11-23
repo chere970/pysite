c722640f9b204d8fb2d6579c2d73cbac299d36c2
cherinet <cherinetkebede055@gmail.com>
2025-11-23 16:02:39 +0300

    Add product detail endpoint and update serializers

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('products/', views.Prodict_list, name='product-list'),
+
+    path('products/<int:pk>/', views.Product_detail, name='product-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,20 @@
-from django.http import JsonResponse
+# from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from api.serializer import ProductSerializer
 from api.models import Product
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
 def Prodict_list(requiest):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return JsonResponse({
-        "data": serializer.data
-    })
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def Product_detail(requiest, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
